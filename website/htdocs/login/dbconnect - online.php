3804c80d0197670a0799076e1bb859e6bc895faf
Bharat L Vora <bharatvora814@gmail.com>
2020-06-20 17:14:51 +0530

    update files

--- website/htdocs/login/dbconnect - online.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$host = "sql305.epizy.com";
-	$dbname = "epiz_25440942_binaryballs";
-	$password = "b1c9hw2f";
-	$user = "epiz_25440942";
-	$conn = mysqli_connect($host,$user,$password,$dbname);
-?>
--- website/htdocs/login/dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$host = "sql205.epizy.com";
-	$dbname = "epiz_25710974_qreward";
-	$password = "6l1w6SVkHMbF";
-	$user = "epiz_25710974";
-	$conn = mysqli_connect($host,$user,$password,$dbname);
-?>
